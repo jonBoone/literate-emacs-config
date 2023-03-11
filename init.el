761dbf10d3563256216c9f92287957797022429b
Jon Boone <ipmonger@delamancha.org>
2023-03-11 17:25:28 -0500

    removes gc control and native complicaton support

--- init.el
+++ init.el
@@ -9,22 +9,6 @@
 
 (require 'cl-lib)                           ; for remove-if
 
-;; Set gc really large, especially when loading the config file
-;; These two lines prevent a stuttering cursor for me, in most cases
-;; FIXME: gc collection in idle time is not the way to do this, but it works for me
-(setq gc-cons-threshold (* 200 1024 1024))
-(run-with-idle-timer 5 t #'garbage-collect)
-
-;; If we have the native compiler, use it
-(message (concat
-          "Native compilation is "
-          (if (and (fboundp 'native-comp-available-p) (native-comp-available-p))
-              (progn
-                (setq comp-deferred-compilation t)
-                "")
-            "*not* ")
-          "available"))
-
 ;; HACK: Disable Org-mode that was shipped with Emacs and add one I control
 (setq load-path (remove-if (lambda (x) (string-match-p "org-20" x))
 			   (remove-if (lambda (x) (string-match-p "org$" x)) load-path)))
