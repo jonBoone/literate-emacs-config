222b2992c23ff551f76100a8970bddf176407d46
Jon Boone <ipmonger@delamancha.org>
2023-05-10 18:13:33 -0400

    updating config files for baseline functionality

--- init.el
+++ init.el
@@ -25,25 +25,9 @@
 (setq load-path (remove-if (lambda (x) (string-match-p "org$" x)) load-path))
 (add-to-list 'load-path "~/.emacs.d/straight/repos/org/lisp")
 
-;; My org file is posted using writefreely, which uses local variables
-;; we need them before the call to org-babel
-(add-to-list 'safe-local-variable-values '(writefreely-post-id . "wf83bq5jwz"))
-(add-to-list 'safe-local-variable-values '(writefreely-post-token . nil))
-
-;; config-file var gets used in mrb.el as well, not sure I like that
-(setq config-file (expand-file-name "mrb.org" user-emacs-directory))
+(setq config-file (expand-file-name "Emacs.org" user-emacs-directory))
 
 ;; This produces mrb.el which is then loaded. It checks datetime before tangling.
 (org-babel-load-file config-file)
 
 ;; END init.el
-;; Exception 1:
-;; Apparently when disabled functions get enabled, Emacs puts them here
-;;
-
-;;; init.el ends here
-(put 'dired-find-alternate-file 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'list-timers 'disabled nil)
