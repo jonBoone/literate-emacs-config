a7da9327a291fa51df2a962e9360329e4623cc48
Jon Boone <ipmonger@delamancha.org>
2023-08-12 21:55:09 -0400

    Modularize configuration (#1)
    * clear out the old files and add Emacs.org
    
    * added Emacs.org
    
    * initial init.el file
    
    * updating config files for baseline functionality
    
    * removed dependency on cl package
    
    * updated straight configuration
    
    * updating .gitignore
    
    * adding updates
    
    * including bootstrap for straight and use-package
    
    * updated with missing information
    
    * updating to ignore elpa directory
    
    * updated custom configurations
    
    * updated with dwilli org-mode config
    
    * correct hook usage
    
    * corrected indentation
    
    * updated to use straight for diminish package
    
    * removing unintentional custoizations
    
    * define variables for font sizes based on value of system-type
    
    * adds with-system macro
    
    * don't fold org content on startup
    
    * add journal file entry
    
    * corrects syntax issues and initialized org-roam
    
    * updates with new settings
    
    * removing org-agenda-files-regexp setting
    
    * increasing font size on linux and enabling github in doom-mode-line
    
    * updated for darwin machine
    
    * update config for better fit on darwin hosts
    
    * updates sly-contribs and font-size for darwin systems
    
    * updates formatting and removes gaps between frames and other windows
    
    * updated to specify sbcl.core-for-sly location
    
    * updating with markdown support
    
    * correct fill-column for org-mode and indentation
    
    * Add dashboard
    
    * Adds dashboard support
    
    * re-organizing some items
    
    * modified org settings for file and used ALL_CAPS for org-keywords
    
    * disable popper and don't use auto-fill-mode in org-mode
    
    * updated to include dashboard icon and fix broken parenthization
    
    * updating spacing
    
    * attempting os-neutral directory specification
    
    * updates sbcl-latest core-file-image location
    
    * updates key bindings and org-agenda-files location
    
    * adds support for dash.el
    
    * adds support for ht.el (hashtables)
    
    * adds s.el (string library)
    
    * adds ts.el (timestamp and date-time library)
    
    * reorganized sections -- added org-super-agenda and dependencies
    
    * adds comment-tags
    
    * updating configuration for org-mode
    
    * adds metrics section for org-mode buffers
    
    * re-write the introduction
    
    * remove blank line
    
    * remove unused content
    
    * update the information on the init files

---
+++ custom.el
@@ -0,0 +1,108 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(connection-local-criteria-alist
+   '(((:application tramp :protocol "flatpak")
+      tramp-container-connection-local-default-flatpak-profile)
+     ((:application tramp)
+      tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
+ '(connection-local-profile-alist
+   '((tramp-container-connection-local-default-flatpak-profile
+      (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin"))
+     (tramp-connection-local-darwin-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . tramp-ps-time)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-busybox-ps-profile
+      (tramp-process-attributes-ps-args "-o" "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "stat=abcde" "-o" "ppid,pgid,tty,time,nice,etime,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (user . string)
+       (group . string)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (ttname . string)
+       (time . tramp-ps-time)
+       (nice . number)
+       (etime . tramp-ps-time)
+       (args)))
+     (tramp-connection-local-bsd-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (group . string)
+       (comm . 52)
+       (state . string)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . number)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-default-shell-profile
+      (shell-file-name . "/bin/sh")
+      (shell-command-switch . "-c"))
+     (tramp-connection-local-default-system-profile
+      (path-separator . ":")
+      (null-device . "/dev/null"))))
+ '(ignored-local-variable-values
+   '((elisp-lint-indent-specs
+      (describe . 1)
+      (it . 1)
+      (thread-first . 0)
+      (cl-flet . 1)
+      (cl-flet* . 1)
+      (org-element-map . defun)
+      (org-roam-dolist-with-progress . 2)
+      (org-roam-with-temp-buffer . 1)
+      (org-with-point-at . 1)
+      (magit-insert-section . defun)
+      (magit-section-case . 0)
+      (org-roam-with-file . 2))
+     (elisp-lint-ignored-validators "byte-compile" "package-lint")))
+ '(org-agenda-files
+   '("/home/iain/Dropbox/pkb/org/journal/2023-07-07-agenda.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -3,59 +3,59 @@
 ;; Emacs initialization file
 ;;
 ;; As a literate emacs configuration, we leverage org-babel.
-;; Our goal here is to load a proper subset of org-mode to enable the full configuration.
+;; Our goal here is to load a proper subset of org-mode to
+;; enable the full configuration.
 
 ;;; Code:
 
-;; upon startup, do the following *after* =early-init.el= is loaded
-;; reset file-name-handler-alist
-(add-hook 'emacs-startup-hook
-	  (lambda ()
-	    (setq file-name-handler-alist file-name-handler-alias-original)
-	    (makunbound 'file-name-hanlder-alist-original)))
+(require 'cl-lib) ; for 'cl-remove
+
+;; ensure =user-init-file= and =user=emacs-directory= are correct
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq user-emacs-directory (file-name-directory user-init-file))
+
 
 ;; force custom into separate local file
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (load custom-file 'noerror)
 
 ;; ensure that we don't pick up the included version of org-mode
-(require 'cl-lib)                           ; for cl-remove
 (setq load-path (cl-remove "org$" load-path :test 'string-match-p))
 
 ;; setup straight.el for package management
-  (defvar bootstrap-version)
-  (setq straight-repository-branch "develop") ; Need this for new org-contrib location
-  (let ((bootstrap-file
-         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-        (bootstrap-version 5))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-           'silent 'inhibit-cookies)
-        (goto-char (point-max))
-        (eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
+(defvar bootstrap-version)
+(setq straight-repository-branch "develop") ; Need this for new org-contrib location
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
 ;; configure 'use-package macro to use straight.el
-    (setq use-package-always-ensure nil   ; Make sure this is nil, so we do not use package.el
-          use-package-verbose 'debug      ; TODO  use a debug var for all of config?
-          )
-    ;; From this point on we should be able to use `use-package
-    (use-package straight
-      :custom
-      (straight-host-usernames '((github . "jonBoone"))) ; TODO Move to personal information?
-      (straight-vc-git-default-protocol 'ssh)
-      (straight-use-package-by-default t)
-      ;; Make sure packages do not pull in internal org, we pregister org from straight.el
-      (straight-register-package 'org)
-      (straight-register-package 'org-contrib))
+(setq use-package-always-ensure nil   ; Make sure this is nil, so we do not use package.el
+      use-package-verbose 'debug      ; TODO  use a debug var for all of config?
+      )
+;; From this point on we should be able to use `use-package
+(use-package straight
+  :custom
+  (straight-host-usernames '((github . "jonBoone"))) ; TODO Move to personal information?
+  (straight-vc-git-default-protocol 'ssh)
+  (straight-use-package-by-default t)
+  ;; Make sure packages do not pull in internal org, we pregister org from straight.el
+  (straight-register-package 'org)
+  (straight-register-package 'org-contrib))
 
-    ;; FROM THIS POINT use-package should work as intended, i.e. using straight.
+;; FROM THIS POINT use-package should work as intended, i.e. using straight.
 
-    ;; Need to install dependencies of use-package manually, why??
-    (use-package diminish)
-
+;; Need to install dependencies of use-package manually, why??
+(use-package diminish
+  :straight t)
 
 ;; set up latest version of org-mode
 (use-package org
@@ -64,7 +64,7 @@
 (add-to-list 'load-path "~/.emacs.d/straight/repos/org-mode/lisp")
 
 ;; config-file var gets used in iain.el as well, not sure I like that
-(setq config-file (expand-file-name "iain.org" user-emacs-directory))
+(setq config-file (expand-file-name "Emacs.org" user-emacs-directory))
 
 ;; This produces iain.el which is then loaded. It checks datetime before tangling.
 (org-babel-load-file config-file)
